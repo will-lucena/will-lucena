301f12672d77fc2b3c02f972a41ffec8a4c6480e
Will Lucena <will.lucen4@gmail.com>
2023-09-29 18:13:44 -0300

    fix max width

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Header />
   <main
-    class="w-full min-h-screen mx-auto flex flex-col gap-8 px-4 pt-20 bg-neutral-50 dark:bg-neutral-800"
+    class="w-full max-w-5xl min-h-screen mx-auto flex flex-col gap-8 px-4 pt-20 bg-neutral-50 dark:bg-neutral-800"
   >
     <router-view></router-view>
     <Footer />
