19e0e035117a35033c05f5cb1f377fd6f842743c
Will Lucena <will.lucen4@gmail.com>
2023-10-15 11:55:41 -0300

    wip message

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <Header />
+  <!-- <Header />
   <main
     class="w-full max-w-5xl min-h-screen mx-auto flex flex-col gap-8 px-4 pt-20 bg-neutral-50 dark:bg-neutral-800"
   >
     <router-view></router-view>
     <Footer />
+  </main> -->
+  <main
+    class="w-full max-w-5xl min-h-screen mx-auto flex flex-col justify-center items-center gap-8 px-4 pt-20 bg-neutral-50 dark:bg-neutral-800"
+  >
+    <h1 class="text-4xl sm:text-5xl text-neutral-950 dark:text-neutral-50">Em construção</h1>
   </main>
 </template>
 
 <script setup>
-import Footer from '@/components/Footer.vue'
-import Header from '@/components/Header.vue'
+// import Footer from '@/components/Footer.vue'
+// import Header from '@/components/Header.vue'
 </script>
 
 <style lang="scss" scoped></style>
